5a838036c329855d2483d3975f1eeb03b6db3f0a
Enidbii <chebsenid@gmail.com>
2023-07-14 15:56:21 +0300

    webstatic

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,7 +13,7 @@ def do_pack():
     time_now = d_object.strftime("%Y%m%d%H%M%S")
     name_locale = "versions/web_static_{}.tgz".format(time_now)
 
-    result = local("tar -czvf {} web_static".format(name_locale),
+    result = local("tar -czvf {} /web_static".format(name_locale),
                    capture=True)
     if result.succeeded is True:
         return name_locale
