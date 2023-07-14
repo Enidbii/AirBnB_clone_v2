6a4dcc98691591c403bdf4564b7865e0a27a7e06
Enidbii <chebsenid@gmail.com>
2023-07-14 15:53:50 +0300

    webstatic

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,13 +7,14 @@ from datetime import datetime
 
 
 def do_pack():
-    # function to generate .tgz
+    """ function to generate .tgz """
     local("mkdir -p versions")
     d_object = datetime.now()
     time_now = d_object.strftime("%Y%m%d%H%M%S")
     name_locale = "versions/web_static_{}.tgz".format(time_now)
 
-    result = local("tar -czvf {} web_static".format(name_locale), capture=True)
+    result = local("tar -czvf {} web_static".format(name_locale),
+                   capture=True)
     if result.succeeded is True:
         return name_locale
     else:
